c2e4d4ecbb6e6325117c1251024c1663e0144cd1
Nathan <nleopard@gmail.com>
2020-06-28 14:01:12 -0400

    update files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,122 +1,111 @@
-function placeHolder() {
-
-    d3.json("../StarterCode/samples.json").then((data) => {
-        //  Create the Traces
-
-        //console.log(data.samples)
-
-
-        //find the otu_ids in the samples data
-        var dataSamples = data.samples
-
-        var otu = dataSamples.map(data => data.otu_ids)
-
-        var slice = otu.slice(0, 10)
-
-        console.log(slice[0].slice(0, 10))
-
-        // find the sample_values in the samples data
-        var dataSamples = data.samples
-
-        var sampleValues = dataSamples.map(data => data.sample_values)
-
-        var slice2 = sampleValues.slice(0, 10)
-
-        console.log(slice2[0].slice(0, 10))
-
-
-        var trace1 = {
-            x: slice[0].slice(0, 10),
-            y: slice2[0].slice(0, 10),
-            type: "bar",
-            name: "Samples",
-            orientation: "h"
-        };
-
-        // Create the data array for the plot
-        var data = [trace1];
-
-        // Define the plot layout
-        var layout = {
-            title: "Samples",
-            xaxis: { title: "Samples" },
-            yaxis: { title: "Samples" }
-        };
-
-        // Plot the chart to a div tag with id "plot"
-        Plotly.newPlot("bar", data, layout);
-    });
-
-};
-
-
-function metaData(patientInfo) {
-
-    d3.json("../StarterCode/samples.json").then(function(data) {
-
-        var demoInfo = data.metadata
-
-        //var filterDemoInfo = demoInfo.filter(demoInfoObject => demoInfoObject.id == patientInfo)
-
-        var sampleMetaData = d3.select("#sample-metadata")
-
-        sampleMetaData.html("");
-
-        Object.entries(filterDemoInfo).forEach(([key, value]) => sampleMetaData.append('p').append('small')
-            .text(key + ": " + value));
-    });
-
-};
-
-function buildMetadata(sample) {
-
-    // @TODO: Complete the following function that builds the metadata panel
-
-    // Use `d3.json` to fetch the metadata for a sample
-    d3.json("/metadata/" + sample).then(function(response) {
-
-        console.log(response);
-        var metadata = response;
-
-        // Use d3 to select the panel with id of `#sample-metadata`
-        var panel = d3.select("#sample-metadata");
-
-        // Use `.html("") to clear any existing metadata
-        panel.html("");
-
-        // Use `Object.entries` to add each key and value pair to the panel
-        // Hint: Inside the loop, you will need to use d3 to append new
-        // tags for each key-value in the metadata.
-        Object.entries(metadata).forEach(([key, value]) => panel.append('p').append('small').text(key + ": " + value));
-
-    });
-}
-
-
-
 function init() {
 
-    var dropDown = d3.select("#selDataset")
+    var dropDown = d3.select("#selDataset");
 
     d3.json("../StarterCode/samples.json").then(function(data) {
-        //console.log(data);
-
+        console.log(data);
         //find the data for patient id
-        var dataNames = data.names
-
+        var dataNames = data.names;
         dataNames.forEach(names => {
-            dropDown.append("option").text(names).property("values", names)
+            dropDown.append("option").text(names).property("values", names);
         })
-
-        //Create a drop down
-        placeHolder()
-
-        //populate the demographic info
-
-        //select the 
-
 
 
     })
 };
-init()
+
+
+//build graph
+function buildGraph(pID) {
+    d3.json("samples.json").then(function(data) {
+        var samples = data.samples;
+        var resultArray = samples.filter(filteredObj => filteredObj.id == pID)
+        var result = resultArray[0]
+        console.log(resultArray)
+        console.log(result)
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        //create the bubble chart
+        var trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: 'Earth',
+                type: 'heatmap'
+            },
+            text: otu_labels
+        };
+
+        var data = [trace1];
+
+        var layout = {
+            title: "<b>Bubble Chart of Belly Button Bacteria</b><br>(All Bacteria)",
+            showlegend: false,
+            xaxis: {
+                title: "OTU ID"
+            },
+            yaxis: {
+                title: ""
+            }
+        };
+
+        Plotly.newPlot('bubble', data, layout);
+
+        // Prepare a list of objects for sorting
+        var list = [];
+        for (var i = 0; i < otu_ids.length; i++) {
+            // Push each object into the list
+            list.push({ 'otu_ids': otu_ids[i], 'otu_labels': otu_labels[i], 'sample_values': sample_values[i] });
+        }
+
+        // Sort function by object key in array
+
+        var trace2 = {
+            x: list.slice(0, 10).map(record => record.sample_values),
+            y: list.slice(0, 10).map(record => "OTU" + record.otu_ids.toString()),
+            hovertext: list.slice(0, 10).map(record => "(" + record.otu_ids + ", " + record.otu_labels + ")"),
+            type: "bar",
+            orientation: "h"
+        };
+
+        var data = [trace2];
+
+        var layout = {
+            height: 500,
+            width: 500
+        };
+
+        Plotly.newPlot("bar", data, layout);
+    })
+
+}
+
+//build metafunction
+function buildMetaData(pID) {
+
+    d3.json("samples.json").then(function(data) {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(filteredObj => filteredObj.id == pID)
+        var result = resultArray[0]
+        console.log(resultArray)
+        console.log(result)
+        var panel = d3.select("#sample-metadata")
+        panel.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            panel.append("p").text(`${key}: ${value}`);
+        })
+    })
+}
+
+function optionChanged(newSample) {
+    buildMetaData(newSample)
+    buildGraph(newSample)
+}
+
+init();
